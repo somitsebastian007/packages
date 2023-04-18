ddd5983af36f237126e0c6caf54209147ce38e36
somitsebastian007 <74805178+somitsebastian007@users.noreply.github.com>
2023-04-19 00:11:21 +0530

    Create kubectl-linux-ubuntu-install.sh

---
+++ kubectl-linux-ubuntu-install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
