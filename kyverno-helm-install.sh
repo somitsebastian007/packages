c17fbba6d34959cee26122b61c853e386b6246af
somitsebastian007 <74805178+somitsebastian007@users.noreply.github.com>
2023-04-23 17:31:50 +0530

    Update kyverno-helm-install.sh

--- kyverno-helm-install.sh
+++ kyverno-helm-install.sh
@@ -1,3 +1,3 @@
-helm repo add kyverno https://kyverno.github.io/kyverno/
-helm repo update
+sudo helm repo add kyverno https://kyverno.github.io/kyverno/
+sudo helm repo update
 sudo helm install kyverno kyverno/kyverno -n kyverno --create-namespace --set replicaCount=1
